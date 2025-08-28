20438262deeeac382179715bff11aea9a8f8772f
DJ_Theron <97227143+DJTheron@users.noreply.github.com>
2025-08-28 17:50:05 +0200

    Update run-all-tests.sh

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,4 +1,6 @@
 cd ~
 git clone https://github.com/DJTheron/browser-testing-tools.git
 cd browser-testing-tools
+# add code to find the main browser of the is
 chrome basic-tab-opener.html
+# add code to find out whether the browser could block the pop ups by possibly checking what is using the most ram or what processs etc
