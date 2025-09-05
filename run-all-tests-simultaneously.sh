9508f7fc1d668df1d73299674b107b6d35ad3cf4
DJ_Theron <97227143+DJTheron@users.noreply.github.com>
2025-09-05 19:23:23 +0200

    Update run-all-tests-simultaneously.sh

--- run-all-tests-simultaneously.sh
+++ run-all-tests-simultaneously.sh
@@ -1,5 +1,6 @@
 cd ~
 git clone https://github.com/DJTheron/browser-testing-tools.git
 cd browser-testing-tools
-chrome basic-tab-test.html
-firefox basic-tab-test.html
+chrome basic-tab-opener.html
+firefox basic-tab-opener.html
+
